9e7dfeea2495ac4217269c09e729f097e13c5ede
SetCloud <121782834+ChunKitGitHub@users.noreply.github.com>
2026-09-10 14:17:44 +0800

    Add files via upload

--- sp.meta.js
+++ sp.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         虾皮跑任务
 // @namespace    https://viayoo.com/
-// @version      2.1.6
+// @version      2.1.7
 // @description  虾皮任务：领取→抓 get_rw→上传，含时段配额
 // @author       You
 // @run-at       document-start
